ed6f417056b909c48ad008f65b62ad04bc319b49
Daria Gileva <dasha.200101@bk.ru>
2024-10-25 19:58:34 +0300

    Проверка

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
+const cardList = document.querySelector(".places__list");
+const cardItem = cardTemplate.querySelector(".places__item");
 
 // @todo: Функция создания карточки
+function createCard(element) {
+  const cardCopy = cardTemplate.cloneNode(true);
+  const cardItem = cardCopy.querySelector(".places__item");
+  const cardTitle = cardItem.querySelector(".card__title");
+  const cardImage = cardItem.querySelector(".card__image");
+  const deleteButton = cardItem.querySelector(".card__delete-button");
 
-// @todo: Функция удаления карточки
+  cardTitle.textContent = element.name;
+  cardImage.setAttribute("src", element.link);
+  cardImage.setAttribute("alt", element.name);
 
-// @todo: Вывести карточки на страницу
+  // @todo: Функция удаления карточки
+  deleteButton.addEventListener("click", function () {
+    cardItem.remove();
+  });
+
+  cardList.append(cardCopy);
+}
+
+initialCards.forEach(createCard);
